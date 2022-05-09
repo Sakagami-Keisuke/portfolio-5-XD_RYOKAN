31e459846170febb14cb5dc8b1db0f3d6af8e818
Macintosh <macintosh@makkintosshunoMacBook-Pro.local>
2022-05-09 14:07:10 +0900

    modal flatpickr

---
+++ _static/dist/js/flatpickr.js
@@ -0,0 +1,13 @@
+
+flatpickr('#flatpickr',{
+  locale: 'ja' , //日本語化
+  minDate: "today", //今日以前指定不可
+  mode: "multiple", //複数選択
+  dateFormat: "Y-m-d",
+  conjunction: " :: "
+});
+
+
+window.onload = function () {
+	alert("＜<flatpicker.js >＞Loading completed");
+};
